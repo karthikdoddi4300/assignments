cea3b8de1be0bf28bcc36917585cd62d1c0e1630
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-02-08 11:20:33 +0530

    shell-practice

--- word-count.sh
+++ word-count.sh
@@ -2,24 +2,15 @@
 echo "file existence and permissions checking using shell scripting"
 
 files=$(ls)
+ 
 
-if [ -d $1 ]; then
-    echo "$1---is a firectory "
-    echo "$files"
-    exit 1
-
-elif [ -f $1 ]; then
-    echo "$1 is a file"
+if [ -e $1]; then 
+    echo " file  exist"
     exit 1
 fi 
 
-if [! -e $1]; then 
-    echo " file doesnt exist"
-    exit 1
-fi 
-
-if [ ! -r  $1] ; then
-     echo " $1 is not readable "
+if [  -r  $1] ; then
+     echo " $1 is  readable "
 
 fi
 if [ -d $1 ]; then
